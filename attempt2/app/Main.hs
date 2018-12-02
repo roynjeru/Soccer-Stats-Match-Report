8cb9e9fe757fd04d40bd99a9f37bad068bbae6a9
RkNjeru <35582018+RkNjeru@users.noreply.github.com>
2018-12-02 14:51:05 -0500

    Progress on getting javascript to work

--- attempt2/app/Main.hs
+++ attempt2/app/Main.hs
@@ -6,21 +6,43 @@
 -- main = someFunc
 
 {-# LANGUAGE OverloadedStrings #-}
+-- {-#LANGUAGE ExtendedDefaultRules#-}
+-- {-#LANGUAGE NoMonomorphismRestriction#-}
+
 module Main where
 
 import Web.Spock
 import Web.Spock.Config
 import Web.Spock.Lucid (lucid)
 import Lucid
+-- import Lucid.Base
 
 import Control.Monad.Trans
 import Data.IORef
 import Data.Monoid()
 import qualified Data.Text as T
+-- import Data.List (intersperse)
+-- import Data.Foldable (foldMap,Foldable)
 
 data MySession = EmptySession
 -- newType ServerState = ServerState {notes :: IORef [Note]}
 data MyAppState = DummyAppState (IORef Int)
+
+-- class With script  where
+--     -- | With the given element(s), use the given attributes.
+--     with :: script -- ^ Some element, either @Html a@ or @Html a -> Html a@.
+--          -> [src]
+--          -> a
+
+-- instance TermRaw Text Attribute where
+--   termRawWith key _ value = makeAttribute key value
+
+-- includeJS :: [T.Text] -> Html ()
+-- includeJS = foldMap (\s-> script_ [src_ s] "")
+-- Found: https://github.com/aleator/LambdaLuento/blob/master/Slides.hs
+
+-- myScript :: Html()
+-- myScript = (script_ [ src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ "")
 
 main :: IO ()
 main =
@@ -40,7 +62,8 @@ app =
                 meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
                 title_ "Generate Report"
                 link_ [ rel_ "stylesheet", href_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" ]
-                -- script_ [ src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ ""
+                --(toHtml "<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>")
+                --script_ [ src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ ""
                 -- script_ [ src_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ] $ ""
             body_ $ do
                 nav_ [ class_ "navbar navbar-default" ] $ do
@@ -51,6 +74,9 @@ app =
             div_ [id_ "mainDiv", class_ "container"] $ do
                 h1_ "Hello!"
                 p_  "Welcome to the Soccer Stats Match Report Application"
+            -- script_ [ src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ ""
+            -- (html "<h1>Hello!<h1>")
+            ""
        get ("hello" <//> var) $ \name ->
            do (DummyAppState ref) <- getState
               visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
@@ -65,8 +91,9 @@ app =
                 meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
                 title_ "Generate Report"
                 link_ [ rel_ "stylesheet", href_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" ]
-                -- script_ [ src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ ""
-                -- script_ [ src_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ] $ ""
+                -- includeJS [ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" 
+                --     , "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ]
+                    
             body_ $ do
                 nav_ [ class_ "navbar navbar-default" ] $ do
                     ul_ [ class_ "nav navbar-nav" ] $ do
@@ -86,8 +113,8 @@ app =
                 meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1"]
                 title_ "Generate Report"
                 link_ [ rel_ "stylesheet", href_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" ]
-                -- script_ [ src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ ""
-                -- script_ [ src_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ] $ ""
+                -- script_ [ type_ "text/javascript", src_ "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" ] $ ""
+                --script_ [ src_ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ] $ ""
             body_ $ do
                 nav_ [ class_ "navbar navbar-default" ] $ do
                     ul_ [ class_ "nav navbar-nav" ] $ do
@@ -97,5 +124,5 @@ app =
             div_ [ id_ "mainDiv", class_ "container" ] $ do
                 a_ [ id_ "linkToPage", href_ "attempt2/app/" ] $ "Link"
                 a_ [ id_ "otherLink", class_ "btn btn-primary", href_ "ExamplePage" ] $ "Other Link"
-                input_ [id_ "inputValue"]                
+                input_ [id_ "inputValue"] 
             ""
